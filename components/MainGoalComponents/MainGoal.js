249466e393575f01647410b92e0fdadb18faa2bf
David Beninson <116832309+David-Beninson@users.noreply.github.com>
2023-04-17 21:39:52 +0300

    updating input

--- components/MainGoalComponents/MainGoal.js
+++ components/MainGoalComponents/MainGoal.js
@@ -57,11 +57,11 @@ export default function MainGoal({
                 close
               </button>
               <input
-                className="p-2.5 bg-gray-50 border border-gray-300 text-gray-300 text-sm rounded-lg focus:ring-green-100 focus:border-green-100 block w-full "
+                className="border-2  border-gray-400 rounded-md px-4 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
                 id="input"
                 type="text"
                 name="title"
-                placeholder="Add a big goal"
+                placeholder="Add an end goal"
                 required
               />
               <button className={styles.goalButton} type="submit">
