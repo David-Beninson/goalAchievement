061334582231ad942d25958837a99452ba55e412
David Beninson <116832309+David-Beninson@users.noreply.github.com>
2023-04-20 01:00:48 +0300

    updating style

--- components/StepsForgoal/Steps.js
+++ components/StepsForgoal/Steps.js
@@ -11,14 +11,20 @@ export default function Steps({
   updateStep,
 }) {
   return (
-    <div>
+<div>
       {goals.length === 0 ? (
-        <div>There are no goals yet</div>
+        <div className="flex items-center py-32 place-content-center">
+          There are no goals yet
+        </div>
       ) : !showStepGoal ? (
-        <div>Select a goal from the list</div>
+        <div className="flex items-center py-32 place-content-center">
+          Select a goal from the list
+        </div>
       ) : (
         <div>
-          <h3>Add a new step for {selectedGoalTitle}:</h3>
+          <h3 className="flex justify-center">
+            Add a new step for {selectedGoalTitle}:
+          </h3>
           <StepForGoal
             selectedGoalId={selectedGoalId}
             updateStep={updateStep}
