9c004c7a39f022eab3bfddf031466f8504121306
David-Beninson <davidbeninson@icloud.com>
2023-04-20 10:11:32 +0300

    implement Website Preview

--- layout/layout.js
+++ layout/layout.js
@@ -14,6 +14,10 @@ export default function Layout({ children }) {
           property="og:image"
           content="https://goal-achievement.vercel.app/ImageUrl.png"
         />
+        <meta
+          property="og:url"
+          content="https://goal-achievement.vercel.app/Login"
+        />
         <meta property="og:image:width" content="200" />
         <meta property="og:image:height" content="200" />
       </Head>
